9b22e005a478d47f67cd0128796be5f31c14ffa4
Randy Faison <randyfaison@gmail.com>
2021-07-08 09:20:04 -0400

    home route

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -78,5 +78,31 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+router.get('/homepage', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 module.exports = router;
